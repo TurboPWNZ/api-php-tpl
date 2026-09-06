d0cc37aa347bc36b99bbe72cbb7dd2ecb38af9fa
kaspruk <s.kaspruk@msl.ua>
2026-09-06 19:21:00 +0000

    #1.0

--- index.php
+++ index.php
@@ -5,6 +5,36 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use FastRoute\Dispatcher;
 
+require_once __DIR__ . '/src/components/Log.php';
+
+// Не показывать ошибки клиентам: пишем в лог и отдаём JSON 500
+error_reporting(E_ALL);
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
+ini_set('log_error_initialization', '1');
+ini_set('log_error_file', __DIR__ . '/logs/php_errors.log');
+
+set_exception_handler(function (Throwable $e): void {
+    $log = \Api\components\Log::get('debug');
+    $log->error('Unhandled exception', [
+        'class' => get_class($e),
+        'message' => $e->getMessage(),
+        'file' => $e->getFile() . ':' . $e->getLine(),
+        'trace' => $e->getTraceAsString(),
+    ]);
+
+    if (!headers_sent()) {
+        http_response_code(500);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode([
+            'success' => false,
+            'errors' => ['500 internal'],
+        ], JSON_UNESCAPED_UNICODE);
+    }
+
+    exit;
+});
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
